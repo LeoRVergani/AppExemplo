587d2758addbe43899a493898008aa1391f69867
leorvergani <vergani@outlook.com.br>
2024-09-11 23:15:18 -0300

    finish app

--- App.js
+++ App.js
@@ -32,33 +32,54 @@ export default function App() {
                 </View>
 
                 <View style={styles.row}>
-                    <MaterialCommunityIcons name='linkedin' size={30} color="#0077B5" />
+                    <MaterialCommunityIcons name='linkedin' size={24} color="#0077B5" />
                     <Text>linkedin.com/in/leorvergani</Text>
                 </View>
 
-                    <Text style={[styles.title, {marginTop: 15}]}>Resumo</Text>
+                    <Text style={[styles.title, {marginTop: 10}]}>Resumo</Text>
                     <View style={styles.separadorView}></View>
                     <Text style={{textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, alias. Deserunt tempore excepturi autem dolorem ipsa, vitae, doloremque alias quod sed debitis dolore molestias illum architecto aspernatur nesciunt iusto ratione!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, alias. Deserunt tempore excepturi autem dolorem ipsa, vitae, doloremque alias quod sed debitis dolore molestias illum architecto aspernatur nesciunt iusto ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </Text>
 
-                    <Text style={[styles.title, {marginTop: 15}]}>Habilidades</Text>
+                    <Text style={[styles.title, {marginTop: 10}]}>Experiência</Text>
                     <View style={styles.separadorView}></View>
 
-                    <View style={styles.row}>
-                        <View style={styles.badge}>
-                            <Text>JavaScript</Text>
-                        </View>
-                        <View style={styles.badge}>
-                            <Text>JavaScript</Text>
-                        </View>
-                        <View style={styles.badge}>
-                            <Text>JavaScript</Text>
-                        </View>
+                    <Text style={[styles.title, {marginTop: 1, paddingLeft: 5}]}>Desenvolvedor Full-Stack</Text>
+                    <Text style={styles.textBold}>Empresa XYZ</Text>
+                    <Text style={styles.textCinza}>Jan 2020 - Presente</Text>
+                    <Text>- Desenvolvi aplicativos móveis usando React Native.</Text>
+                    <Text>- Testes unitários.</Text>
+                    <Text>- Liderança de equipes.</Text>
+
+                    <Text style={[styles.title, {marginTop: 5, paddingLeft: 5}]}>Desenvolvedor Front-End</Text>
+                    <Text style={styles.textBold}>Empresa ABC</Text>
+                    <Text style={styles.textCinza}>Jan 1976 - Dez 1997</Text>
+                    <Text>- Desenvolvi o HTML junto com Mark Zuckerberg.</Text>
+                    <Text>- Treinei o Bill Gates e sua equipe.</Text>
+                    <Text>- Criei o primeiro boot do Windows 95.</Text>
+
+                    <Text style={[styles.title, {marginTop: 10}]}>Educação</Text>
+                    <View style={styles.separadorView}></View>
+                    <Text style={[styles.title, {marginTop: 1, paddingLeft: 5}]}>Bacharel em Ciência da Computação</Text>
+                    <Text style={styles.textBold}>Universidade do Albert Einstein</Text>
+                    <Text style={styles.textCinza}>1970 - 1978</Text>
+
+                    <Text style={[styles.title, {marginTop: 10}]}>Habilidades</Text>
+                    <View style={styles.separadorView}></View>
+
+                    <View style={styles.listItens}>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>JavaScript</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>Git</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>Python</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>React</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>C++</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>React Native</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>Golang</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>Dart</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>Flutter</Text></View>
+                        <View style={styles.badge}><Text style={styles.bagdeTag}>Node.js</Text></View>
                     </View>
-
-
-
 
                 </ScrollView>
 
@@ -83,6 +104,7 @@ const styles = StyleSheet.create({
         color: '#0077B5',
         alignSelf: 'center',
         fontSize: 28,
+        fontWeight: 'bold'
     },
     myPosition: {
         color: '#CCC000',
@@ -94,10 +116,12 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 2,
         marginVertical: 5,
+        marginBottom: 10
     },
     title:{
         color:'#0077B5',
         fontSize: 20,
+        fontWeight: 'bold'
     },
     row: {
         flexDirection: 'row',
@@ -108,7 +132,33 @@ const styles = StyleSheet.create({
     badge: {
         backgroundColor: '#EAF4FF',
         padding: 5,
-        borderRadius: 5,
-        opacity: 0.5
+        borderRadius: 11,
+        maxWidth: 125,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 10
+    },
+    bagdeTag: {
+        color: '#0077B5',
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    listItens:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        flexWrap: 'wrap',
+        height: 100
+    },
+    textCinza: {
+        color: '#a3a3a3',
+        paddingLeft: 10
+    },
+    textBold: {
+        fontWeight: 'bold',
+        color: '#686868',
+        paddingLeft: 10
     }
 })
